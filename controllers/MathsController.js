7d2edc52c19aacf65bde4b3cca7772e021d1bef8
mohas191 <60118548+mohas191@users.noreply.github.com>
2023-10-09 22:07:34 -0400

    Update MathsController.js

--- controllers/MathsController.js
+++ controllers/MathsController.js
@@ -3,10 +3,21 @@ import Repository from '../models/repository.js';
 import Controller from './Controller.js';
 import mathsCalculator from '../models/mathsCalculator.js';
 import HttpContext from '../httpContext.js';
+import fs from "fs";
+import path from "path";
 
 export default class MathsController extends Controller {
+    
     constructor(xHttpContext) {
         super(xHttpContext)//, new Repository(new CourseModel()));
+
+        this.HttpContext = xHttpContext;
+        if(xHttpContext.path.params == null || Object.keys(xHttpContext.path.params).length == 0){
+            this.reponduEnHTML = true;
+            let pageAide = path.join(process.cwd(), wwwroot, '/mathsHelp.html');
+            this.HttpContext.response.HTML(fs.readFileSync(pageAide));
+            return;
+        }
         this.setup(xHttpContext);
     }
     setup(xHttpContext){
@@ -16,6 +27,7 @@ export default class MathsController extends Controller {
         this.mc = m;
     }
     get(){
+        if(this.reponduEnHTML){return;}
         let m = this.mc;
         let tab = {};
         tab.x = m.x;
